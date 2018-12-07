103c3b126018a135dd98c64f497c190ed04ac74b
Sukan Singh <sukansingh@gmail.com>
2018-12-08 00:21:29 +0530

    added TestNGRunner in src/main

--- src/test/java/com/demo/testcases/GetUsers.java
+++ src/test/java/com/demo/testcases/GetUsers.java
@@ -1,5 +1,6 @@
 package com.demo.testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetUsers {
@@ -7,15 +8,16 @@ public class GetUsers {
 	@Test
 	public void getAllUsers(){
 		
-		System.out.println(" ############################ START ############################");
+		System.out.println("############################ START ############################");
 		System.out.println("getAllUsers test case");
+		
 		System.out.println(" ############################ END ############################");
 	}
 	
 	@Test
 	public void getUserById(){
 		
-		System.out.println(" ############################ START ############################");
+		System.out.println("############################ START ############################");
 		System.out.println("getUserById test case");
 		System.out.println(" ############################ END ############################");
 	}
