4a0dc84982edca04e4d64e66e153a632bb041861
sukansingh <sukansingh@gmail.com>
2018-12-14 18:47:39 +0530

    Create AutomationTest.java

---
+++ src/test/java/com/demo/testcases/AutomationTest.java
@@ -0,0 +1,32 @@
+package com.demo.testcases;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class AutomationTest {
+
+	@Test
+	public void getAllUsers1(){
+		
+		System.out.println("############################ START ############################");
+		System.out.println("getAllUsers1 test case");
+		
+		System.out.println(" ############################ END ############################");
+	}
+	
+	@Test
+	public void getAllUsers2(){
+		
+		System.out.println("############################ START ############################");
+		System.out.println("getAllUsers2 test case");
+		System.out.println(" ############################ END ############################");
+	}
+  @Test
+	public void getAllUsers3(){
+		
+		System.out.println("############################ START ############################");
+		System.out.println("getAllUsers3 test case");
+		System.out.println(" ############################ END ############################");
+    Assert.assertEquals("True", "False")
+	}
+}
